0479cc0f87f1ca1b6afa8da126c01d9f78452525
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-08-23 08:37:14 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lista21082017ex07
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			decimal nota1, nota2, nota3, media;
+
+			Console.Write("Digite a nota 1: ");
+			nota1 = Convert.ToDecimal(Console.ReadLine());
+			Console.Write("Digite a nota 2: ");
+			nota2 = Convert.ToDecimal(Console.ReadLine());
+			Console.Write("Digite a nota 3: ");
+			nota3 = Convert.ToDecimal(Console.ReadLine());
+
+            media = ((nota1 * 2) + (nota2 * 3) + (nota3 * 5)) / 10;
+			
+			Console.Write("A média final é {0:##,##0.00}", media);
+		
+			Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Lista21082017ex07 C# Curso Programação de Dispositivos Móveis SENAC
